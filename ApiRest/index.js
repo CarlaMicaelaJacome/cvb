45f54415397d97d7340155982ca24cabc58767aa
Carla Micaela Jacome <mika_jacome@hotmail.com>
2023-04-29 01:18:59 +0200

    create files

--- ApiRest/index.js
+++ ApiRest/index.js
@@ -1,19 +1,26 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-const app = express ()
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const connect =async () =>{
-    try{
-        await mongoose.connect(process.env.MONGO);
-        console.log("Connected to MongoDB!")
-    } catch (error){
-        throw error;
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO);
+      console.log("Connected to mongoDB.");
+    } catch (error) {
+      throw error;
     }
 };
 
-app. listen (8800, ()=>{
-    connect()
-    console.log("Connected to backend")
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+  });
+
+app.listen(8800, () => {
+    connect();
+    console.log("Connected to backend.");
 });
